b1b9bf6316eb044f5073f8b90a0ed0540395dbf3
Yaroslav <116502429+yaragirodev@users.noreply.github.com>
2026-02-09 03:56:52 +0300

    music player

--- global/globalData.gd
+++ global/globalData.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var cubesTotal = 0
+var isMusicPlaying = false
--- gui/gui.gd
+++ gui/gui.gd
@@ -2,14 +2,17 @@ extends Control
 
 onready var spawnButton = $spawnCubeButton
 onready var deleteButton = $deleteAllButton
+onready var musicButton = $MusicButton
 onready var spawned_spatial = mainLvl.get_node("spawnedStuff")
 onready var cubesCount: Label = $CubesCount
+onready var musicPlayer = mainLvl.get_node("musicStream")
 
 var boxScene = preload("res://assets/cube/cube.tscn")
 
 func _ready():
 	spawnButton.connect("pressed", self, "_onSpawnButtonPressed")
 	deleteButton.connect("pressed", self, "_onDeleteButtonPressed")
+	musicButton.connect("pressed", self, "_onMusicButtonPressed")
 	
 func _process(delta):
 	cubesCount.text = str(globalData.cubesTotal)
@@ -35,4 +38,14 @@ func addOneToGlobalCount():
 	globalData.cubesTotal += 1
 	
 
+func _onMusicButtonPressed():
+	if globalData.isMusicPlaying == false:
+		musicPlayer.play()
+		globalData.isMusicPlaying = true
+		musicButton.text = "Music ON"
+		
+	else:
+		musicPlayer.stop()
+		globalData.isMusicPlaying = false
+		musicButton.text = "Music OFF"
 
