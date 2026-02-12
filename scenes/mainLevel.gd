be2430857876e3b0b0c741216b2d265084c2788d
Yaroslav <116502429+yaragirodev@users.noreply.github.com>
2026-02-13 01:04:39 +0300

    new optimization of optimization

--- scenes/mainLevel.gd
+++ scenes/mainLevel.gd
@@ -2,19 +2,21 @@ extends Spatial
 
 onready var light1 = $env/SpotLight2
 onready var spawned_spatial = get_node("spawnedStuff")
+var isVita = false
 
 func _ready():
+	# is this vita?
+	isVita = OS.get_name() == "Vita"
+	# no shadows on vita (its bugged)
 	if OS.get_name() == "Vita":
 		light1.shadow_enabled = false
 		
 
 # i think this will help vita players
 func _process(delta):
-	# if player on vita
-	if OS.get_name() == "Vita":
-		if globalData.cubesTotal >= 100:
-			# OH NO 100 CUBES!
-			globalData.cubesTotal = 0
-			# delete all cubes ):
-			for child in spawned_spatial.get_children():
-				child.queue_free()
+	if isVita and globalData.cubesTotal >= 100:
+		# OH NO 100 CUBES!
+		globalData.cubesTotal = 0
+		# delete all cubes ):
+		for child in spawned_spatial.get_children():
+			child.queue_free()
