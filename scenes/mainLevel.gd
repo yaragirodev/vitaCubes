a22696a96ee254bed46c8f9e178a866c5c355386
Yaroslav <116502429+yaragirodev@users.noreply.github.com>
2026-02-18 03:07:33 +0300

    optimization, invisible wall

--- scenes/mainLevel.gd
+++ scenes/mainLevel.gd
@@ -1,12 +1,14 @@
 extends Spatial
 
-onready var light1 = $env/SpotLight2
+onready var light1 = $env/OmniLight
 onready var spawned_spatial = get_node("spawnedStuff")
 var isVita = false
+var OnPC = false
 
 func _ready():
 	# is this vita?
 	isVita = OS.get_name() == "Vita"
+	OnPC = OS.get_name() == "Windows"
 	# no shadows on vita (its bugged)
 	if OS.get_name() == "Vita":
 		light1.shadow_enabled = false
@@ -17,5 +19,9 @@ func _process(delta):
 		# OH NO 100 CUBES!
 		globalData.cubesTotal = 0
 		# delete all cubes ):
+		for child in spawned_spatial.get_children():
+			child.queue_free()
+			
+	if Engine.get_frames_per_second() <= 20:
 		for child in spawned_spatial.get_children():
 			child.queue_free()
